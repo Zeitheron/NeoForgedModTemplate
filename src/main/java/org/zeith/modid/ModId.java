4766556c25932c48991be4f79b3ed2960f4ade1a
Zeith <me@zeith.org>
2025-03-05 01:03:19 +0200

    1.21.4

--- src/main/java/org/zeith/modid/ModId.java
+++ src/main/java/org/zeith/modid/ModId.java
@@ -1,15 +1,22 @@
 package org.zeith.modid;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.javafmlmod.FMLModContainer;
+import org.slf4j.*;
 import org.zeith.hammerlib.api.items.CreativeTab;
 import org.zeith.hammerlib.core.adapter.LanguageAdapter;
 import org.zeith.hammerlib.core.init.ItemsHL;
 import org.zeith.hammerlib.proxy.HLConstants;
+import org.zeith.hammerlib.util.mcf.Resources;
 
-@Mod(ModId.MOD_ID)
+@Mod(org.zeith.modid.ModId.MOD_ID)
 public class ModId
 {
+	public static final Logger LOG = LoggerFactory.getLogger(org.zeith.modid.ModId.class);
+	
 	public static final String MOD_ID = "modid";
 	
 	@CreativeTab.RegisterTab
@@ -19,13 +26,13 @@ public class ModId
 					.withTabsBefore(HLConstants.HL_TAB.id())
 	);
 	
-	public ModId()
+	public ModId(FMLModContainer container, IEventBus modEventBus, Dist dist)
 	{
 		LanguageAdapter.registerMod(MOD_ID);
 	}
 	
 	public static ResourceLocation id(String path)
 	{
-		return new ResourceLocation(MOD_ID, path);
+		return Resources.location(MOD_ID, path);
 	}
 }
--- src/main/java/org/zeith/modid/init/ItemsMI.java
+++ src/main/java/org/zeith/modid/init/ItemsMI.java
@@ -1,13 +1,9 @@
 package org.zeith.modid.init;
 
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import org.zeith.hammerlib.annotations.RegistryName;
-import org.zeith.hammerlib.annotations.SimplyRegister;
+import net.minecraft.world.item.context.UseOnContext;
+import org.zeith.hammerlib.annotations.*;
 import org.zeith.modid.ModId;
 
 @SimplyRegister
@@ -17,10 +13,10 @@ public interface ItemsMI
 	Item TEST_ITEM = ModId.MOD_TAB.add(new Item(new Item.Properties())
 	{
 		@Override
-		public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand)
+		public InteractionResult useOn(UseOnContext context)
 		{
-			System.out.println(player.getItemInHand(hand));
-			return InteractionResultHolder.success(player.getItemInHand(hand));
+			System.out.println(context.getItemInHand());
+			return InteractionResult.SUCCESS;
 		}
 	});
 }
